89a22f5ff1474e3bf429b728f25363375c2422fe
Oluwatimilehin Alo <timisnup@gmail.com>
2022-08-15 01:45:53 +0100

    first commit

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance" {
   ami                         = var.instance_ami
-  key_name                    = "goodclass"
+  key_name                    = "timmie"
   instance_type               = "t2.micro"
   availability_zone           = "eu-west-2a"
   associate_public_ip_address = true
